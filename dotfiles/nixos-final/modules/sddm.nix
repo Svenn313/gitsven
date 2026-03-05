5fa50094e93ba2f8c4070f493f77884e682b23d5
Sven <Sven313@protonmail.com>
2026-03-05 19:58:24 +0100

    add some style to sddm

--- dotfiles/nixos-final/modules/sddm.nix
+++ dotfiles/nixos-final/modules/sddm.nix
@@ -1,24 +1,107 @@
 { config, lib, pkgs, ... }:
+
+let
+  sddmTheme = pkgs.sddm-astronaut.override {
+    embeddedTheme = "black_hole";
+    themeConfig = {
+      FullBlur = "false";
+      PartialBlur = "true";
+      BlurMax = "32";
+      Blur = "0.3";
+
+      RoundCorners = "10";
+
+      HourFormat = "HH:mm";
+      DateFormat = "dddd d MMMM";
+      Locale = "fr_FR";
+
+      FormPosition = "left";
+      HaveFormBackground = "true";
+
+      # Catppuccin Mocha
+      FormBackgroundColor = "#1e1e2e";
+      BackgroundColor = "#cba6f7";
+      DimBackgroundColor = "#000000";
+      DimBackground = "0.3";
+
+      HeaderTextColor = "#cdd6f4";
+      DateTextColor = "#cdd6f4";
+      TimeTextColor = "#cba6f7";
+
+      LoginFieldBackgroundColor = "#313244";
+      PasswordFieldBackgroundColor = "#313244";
+      LoginFieldTextColor = "#cdd6f4";
+      PasswordFieldTextColor = "#cdd6f4";
+      UserIconColor = "#cba6f7";
+      PasswordIconColor = "#cba6f7";
+
+      PlaceholderTextColor = "#6c7086";
+      WarningColor = "#f38ba8";
+
+      LoginButtonTextColor = "#1e1e2e";
+      LoginButtonBackgroundColor = "#cba6f7";
+      SystemButtonsIconsColor = "#cdd6f4";
+      SessionButtonTextColor = "#cdd6f4";
+
+      DropdownTextColor = "#cdd6f4";
+      DropdownSelectedBackgroundColor = "#cba6f7";
+      DropdownBackgroundColor = "#181825";
+
+      HighlightTextColor = "#1e1e2e";
+      HighlightBackgroundColor = "#cba6f7";
+      HighlightBorderColor = "#b4befe";
+
+      HoverUserIconColor = "#b4befe";
+      HoverPasswordIconColor = "#b4befe";
+      HoverSystemButtonsIconsColor = "#b4befe";
+      HoverSessionButtonTextColor = "#b4befe";
+
+      Background = "/etc/sddm-wallpaper";
+    };
+  };
+
+  # Script qui copie un wallpaper aléatoire dans /etc/sddm-wallpaper
+  sddmWallpaperScript = pkgs.writeShellScript "sddm-wallpaper" ''
+    WALLPAPER_DIR="/home/sven/Pictures/Wallpapers"
+    WALLPAPERS=($(ls "$WALLPAPER_DIR"/*.{jpg,jpeg,png,webp} 2>/dev/null))
+    COUNT=''${#WALLPAPERS[@]}
+    if [ "$COUNT" -gt 0 ]; then
+      RANDOM_INDEX=$((RANDOM % COUNT))
+      cp "''${WALLPAPERS[$RANDOM_INDEX]}" /etc/sddm-wallpaper
+    fi
+  '';
+in
 {
   options.modules.sddm.enable = lib.mkEnableOption "sddm";
   config = lib.mkIf config.modules.sddm.enable {
+
+    # Service qui lance le script avant SDDM
+    systemd.services.sddm-wallpaper = {
+      description = "Set random SDDM wallpaper";
+      wantedBy = [ "sddm.service" ];
+      before = [ "sddm.service" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${sddmWallpaperScript}";
+      };
+    };
+
     services.displayManager.sddm = {
       enable         = true;
       wayland.enable = true;
-      theme          = "sddm-astronaut-theme";
+      theme          = "${sddmTheme}/share/sddm/themes/sddm-astronaut-theme";
       settings.General.InputMethod = "";
       extraPackages = [
         pkgs.kdePackages.qtmultimedia
-        (pkgs.sddm-astronaut.override { embeddedTheme = "black_hole"; })
+        sddmTheme
       ];
     };
+
     services.xserver.xkb = {
       layout  = "fr";
       variant = "azerty";
     };
-    environment.systemPackages = [
-      (pkgs.sddm-astronaut.override { embeddedTheme = "black_hole"; })
-    ];
+
     environment.pathsToLink = [ "/share/sddm/themes" ];
   };
 }
