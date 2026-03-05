bd19b9162e29de15cedb0a6dc9836694b95864d8
Sven <Sven313@protonmail.com>
2026-03-05 14:00:24 +0100

    add noctalia SDDM into his own file for clarity

--- dotfiles/nixos-final/modules/sddm.nix
+++ dotfiles/nixos-final/modules/sddm.nix
@@ -1,26 +1,31 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.displayManager.sddm = {
-    enable         = true;
-    wayland.enable = true;
-    settings.General.InputMethod = "";
-    theme = "noctalia";
-  };
+  options.modules.sddm.enable = lib.mkEnableOption "sddm";
 
-  environment.systemPackages = [
-    (pkgs.stdenv.mkDerivation {
-      name = "sddm-noctalia-theme";
-      src = pkgs.fetchFromGitHub {
-        owner  = "mahaveergurjar";
-        repo   = "sddm";
-        rev    = "noctalia";
-        sha256 = pkgs.lib.fakeHash;
-      };
-      installPhase = ''
-        mkdir -p $out/share/sddm/themes/noctalia
-        cp -r . $out/share/sddm/themes/noctalia
-      '';
-    })
-  ];
+  config = lib.mkIf config.modules.sddm.enable {
+
+    services.displayManager.sddm = {
+      enable         = true;
+      wayland.enable = true;
+      settings.General.InputMethod = "";
+      theme = "noctalia";
+    };
+
+    environment.systemPackages = [
+      (pkgs.stdenv.mkDerivation {
+        name = "sddm-noctalia-theme";
+        src = pkgs.fetchFromGitHub {
+          owner  = "mahaveergurjar";
+          repo   = "sddm";
+          rev    = "noctalia";
+          sha256 = pkgs.lib.fakeHash;
+        };
+        installPhase = ''
+          mkdir -p $out/share/sddm/themes/noctalia
+          cp -r . $out/share/sddm/themes/noctalia
+        '';
+      })
+    ];
+  };
 }
