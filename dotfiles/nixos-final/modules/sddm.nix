7f2234192278c796ddd3bb070c6128b36adef530
Sven <Sven313@protonmail.com>
2026-03-05 19:40:33 +0100

    add black-hole theme for sddm

--- dotfiles/nixos-final/modules/sddm.nix
+++ dotfiles/nixos-final/modules/sddm.nix
@@ -1,29 +1,23 @@
 { config, lib, pkgs, ... }:
-
 {
   options.modules.sddm.enable = lib.mkEnableOption "sddm";
-
   config = lib.mkIf config.modules.sddm.enable {
-
     services.displayManager.sddm = {
       enable         = true;
       wayland.enable = true;
       theme          = "sddm-astronaut-theme";
       settings.General.InputMethod = "";
       extraPackages = [
-      pkgs.kdePackages.qtmultimedia
-      (pkgs.sddm-astronaut.override { embeddedTheme = "black_hole"; })
+        pkgs.kdePackages.qtmultimedia
+        (pkgs.sddm-astronaut.override { embeddedTheme = "black_hole"; })
       ];
     };
-
     services.xserver.xkb = {
       layout  = "fr";
       variant = "azerty";
     };
-
     environment.systemPackages = [
-      pkgs.sddm-astronaut
       (pkgs.sddm-astronaut.override { embeddedTheme = "black_hole"; })
     ];
   };
-
+}
