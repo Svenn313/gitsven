e7faefdeef86cdfbe1cad8325adc08f5b9a6f463
Sven <Sven313@protonmail.com>
2026-03-06 11:12:55 +0100

    add nvidia video acceleration

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,15 +1,15 @@
 { config, lib, pkgs, ... }:
-
 {
   options.modules.nvidia.enable = lib.mkEnableOption "nvidia";
-
   config = lib.mkIf config.modules.nvidia.enable {
-
     services.xserver.videoDrivers = [ "nvidia" ];
 
     hardware.graphics = {
       enable      = true;
       enable32Bit = true;
+      extraPackages = with pkgs; [
+        nvidia-vaapi-driver
+      ];
     };
 
     hardware.nvidia = {
@@ -19,7 +19,6 @@
       open                        = false;
       nvidiaSettings              = true;
       package                     = config.boot.kernelPackages.nvidiaPackages.stable;
-
       prime = {
         sync.enable = true;
         intelBusId  = "PCI:0:2:0";
@@ -34,5 +33,9 @@
       NVD_BACKEND               = "direct";
       GBM_BACKEND               = "nvidia-drm";
     };
+
+    environment.systemPackages = with pkgs; [
+      nvidia-vaapi-driver
+    ];
   };
 }
