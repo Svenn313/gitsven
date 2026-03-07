070d0e093188d02a603bdc9e1fb0d84c51f4bf1f
Sven <Sven313@protonmail.com>
2026-03-07 16:00:06 +0100

    lowercasing home folders

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -62,5 +62,19 @@ wayland.windowManager.hyprland.enable = true;
 
   home.username      = "sven";
   home.homeDirectory = "/home/sven";
+
+  xdg.userDirs = {
+  enable = true;
+  createDirectories = true;
+  desktop = "${config.home.homeDirectory}/desktop";
+  documents = "${config.home.homeDirectory}/documents";
+  download = "${config.home.homeDirectory}/downloads";
+  music = "${config.home.homeDirectory}/music";
+  pictures = "${config.home.homeDirectory}/pictures";
+  publicShare = "${config.home.homeDirectory}/public";
+  templates = "${config.home.homeDirectory}/templates";
+  videos = "${config.home.homeDirectory}/videos";
+  };
+
   home.stateVersion  = "24.11";
 }
