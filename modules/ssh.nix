afc9e2a3974f6f5dffec28cf7434472905e65161
Svenn313 <sven313@protonmail.com>
2026-04-16 11:27:36 +0200

    add config file for ssh

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,7 +1,9 @@
 { config, lib, ... }:
 {
   options.modules.ssh.enable = lib.mkEnableOption "ssh";
+
   config = lib.mkIf config.modules.ssh.enable {
+
     services.openssh = {
       enable = true;
       ports  = [ 22923 ];
@@ -12,5 +14,25 @@
         KbdInteractiveAuthentication = false;
       };
     };
+
+    environment.etc."ssh/ssh_config.d/hosts.conf".text = ''
+      Host arch nixos laptop
+        HostName 192.168.1.178
+        User sven
+        Port 22923
+        IdentityFile ~/.ssh/svenKey
+
+      Host laptop-dropbear
+        HostName 192.168.1.178
+        User root
+        Port 2222
+        IdentityFile ${key}
+
+      Host ubuntu
+        HostName 192.168.1.87
+        User sven
+        Port 22923
+        IdentityFile ~/.ssh/svenKey
+    '';
   };
 }
