cb4a9f9cfa104dcd558a403e587bf7cbe0fc7671
Svenn313 <sven313@protonmail.com>
2026-04-16 11:36:15 +0200

    modifying ssh nix file with matchblocks

---
+++ modules/home/sshConfig.nix
@@ -0,0 +1,30 @@
+{ config, ... }:
+{
+  programs.ssh = {
+    enable = true;
+
+    matchBlocks = {
+      "arch" = {
+        host = "arch nixos laptop";
+        hostname = "192.168.1.178";
+        user = "sven";
+        port = 22923;
+        identityFile = "~/.ssh/svenKey";
+      };
+
+      "laptop-dropbear" = {
+        hostname = "192.168.1.178";
+        user = "root";
+        port = 2222;
+        identityFile = "~/.ssh/svenKey";
+      };
+
+      "ubuntu" = {
+        hostname = "192.168.1.87";
+        user = "sven";
+        port = 22923;
+        identityFile = "~/.ssh/svenKey";
+      };
+    };
+  };
+}
