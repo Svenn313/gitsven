157f9bd22c3a98697707120ad5df7ab4be39727f
Svenn313 <sven313@protonmail.com>
2026-05-26 10:56:10 +0200

    migrate noctalia inline config to toml

--- modules/home/noctalia.nix
+++ modules/home/noctalia.nix
@@ -1,72 +1,20 @@
-{ config, pkgs, inputs, ... }:
-
 {
-  programs.noctalia = {
-    settings = {
-      shell = {
-        corner_radius_scale = 1;
-        telemetry_enabled   = false;
-        avatar_path         = "/home/sven/.face";
-        animation.speed     = 1.0;
-      };
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    inputs.noctalia.homeModules.default
+  ];
 
-      theme = {
-        mode    = "dark";
-        source  = "builtin";
-        builtin = "Gruvbox";
-      };
+  config = lib.mkIf (config.desktop.enable && config.desktop.tiling.enable) {
+    programs.noctalia = {
+      enable = true;
 
-      wallpaper = {
-        enabled             = true;
-        directory           = "/home/sven/pictures/wallpapers";
-        fill_mode           = "crop";
-        transition_duration = 1500;
-        automation = {
-          enabled          = true;
-          order            = "random";
-          interval_minutes = 5;
-        };
-      };
-
-      bar = {
-        main = {
-          position           = "top";
-          background_opacity = 0.85;
-          margin_h           = 8;
-          margin_v           = 6;
-          start              = [ "launcher" "clock" "system-monitor" "battery" "active-window" ];
-          center             = [ "workspaces" ];
-          end                = [ "tray" "notifications" "volume" "brightness" "control-center" ];
-        };
-      };
-
-      dock = {
-        enabled   = true;
-        position  = "bottom";
-        auto_hide = true;
-      };
-
-      notification = {
-        enable_daemon = true;
-        offset_x      = 20;
-        offset_y      = 8;
-      };
-
-      idle = {
-        behavior = {
-          lock = {
-            timeout = 660;
-            command = "noctalia:screen-lock";
-            enabled = true;
-          };
-          "screen-off" = {
-            timeout        = 600;
-            command        = "noctalia:dpms-off";
-            resume_command = "noctalia:dpms-on";
-            enabled        = true;
-          };
-        };
-      };
+      settings = builtins.fromTOML (builtins.readFile ./noctalia-config.toml);
     };
   };
 }
