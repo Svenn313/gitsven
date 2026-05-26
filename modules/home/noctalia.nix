c2d8c763b1ab9efdf14c22d6aba4e7d2f586165b
Svenn313 <sven313@protonmail.com>
2026-05-26 11:27:52 +0200

    move noctalia toml and json to their own folder

--- modules/home/noctalia.nix
+++ modules/home/noctalia.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-  programs.noctalia.settings = builtins.fromTOML (builtins.readFile ./noctalia-config.toml);
+  programs.noctalia.settings = builtins.fromTOML (builtins.readFile ./noctalia/noctalia-config.toml);
 
-  xdg.configFile."noctalia/palettes/svenGruvboxPalette.json".source = ./noctalia-palette.json;
+  xdg.configFile."noctalia/palettes/svenGruvboxPalette.json".source = ./noctalia/noctalia-palette.json;
 }
