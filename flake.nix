d7945df01cbb05f1bee25234b9e938365af49d2e
Svenn313 <sven313@protonmail.com>
2026-09-12 12:29:58 +0200

    Pin nixpkgs to nixos-26.05, home-manager to release-26.05

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Sven's NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia = {
